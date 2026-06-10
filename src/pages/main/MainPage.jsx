019493276c5f2e3b124a4b69e064673d7b4862d0
rohit <rohit-singh@pearlorganisation.com>
2026-06-10 17:23:33 +0530

    .

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,3 +1,5 @@
+import React from "react"; 
+
 import { Navigate } from "react-router-dom";
 import Loading from "../../components/loading/Loading";
 import { useAuth } from "../../hooks/AuthProvider";
